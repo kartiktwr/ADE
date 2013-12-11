4dfaf0a4c95437aee2024de5da83d34df0825428
Kartik <kartik@kartik.(none)>
2013-12-10 23:39:08 -0800

    key_input file added

--- board_files/CPP_Version/src/angstrom_avoid_obj.cpp
+++ board_files/CPP_Version/src/angstrom_avoid_obj.cpp
@@ -8,39 +8,56 @@
 #include <iostream>
 #include "Hexapod.h"
 #include "Serial.h"
+#include "pubsub.h"
 
 using namespace std;
 
 Hexapod robot;
+//char UP, DOWN, LEFT, RIGHT;
 bool setup() {
 	robot.Hexapod_init();
+/*	cout << "Press the UP key" << endl;
+	UP = getchar();
+	cout << "UP key registered" << endl;
+	cout << "Press the DOWN key" << endl;
+	DOWN = getchar();
+	cout << "DOWN key registered" << endl;
+	cout << "Press the LEFT key" << endl;
+	LEFT = getchar();
+	cout << "LEFT key registered" << endl;
+	cout << "Press the RIGHT key" << endl;
+	RIGHT = getchar();
+	cout << "RIGHT key registered" << endl;
+	cout << "Begin you moves";*/
 	return true;
 }
 
+char data[1];
 bool loop(){
-	if(kbhit()){
-		char ch = getchar();
-		switch (ch){
-		case 'w':
+	key_t key = 1234;
+	if(subscribe(key, data, 1)){
+//		char ch = getchar();
+		if (data[0] == 'w'){
 			robot.walkFwd(1);
-//			cout << 65;
 			return true;
-		case 's':
-			robot.walkFwd(-1);
-//			cout << 66;
-			return true;
-		case 'a':
-			robot.walkSide(1);
-//			cout << 67;
-			return true;
-		case 'd':
-			robot.walkSide(-1);
-//			cout << 68;
-			return true;
-		case 'c':
+		}
+		if (data[0] == 's'){
+			robot.walkFwd(-1);    
+                        return true;
+		}
+		if (data[0] == 'a'){
+			robot.walkSide(1);     
+                        return true;
+		}
+		if (data[0] == 'd'){
+			robot.walkSide(-1);    
+                        return true;
+		}
+		if (data[0] == 27){
+			cout << "end" << endl;
 			robot.reliefStep();
-//			delete robot;
-			return false;
+			endTransfer(key); 
+                        return false;
 		}
 	}
 	return true;
